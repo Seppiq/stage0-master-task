c72f7fc561abcb68068c382f93d157cb86647924
Kirill Semak <mvrch31@gmail.com>
2023-03-13 19:57:16 +0300

    feat: add all Conditions tasks, Loops

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -1,9 +1,27 @@
 package com.epam.conditions;
 
+
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
+        if (year < 0 || month < 1 || month > 12) {
+            System.out.println("invalid date");
+            return;
+        }
 
+        int daysInMonth;
+        switch (month) {
+            case 2 -> {
+                if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+                    daysInMonth = 29;
+                } else {
+                    daysInMonth = 28;
+                }
+            }
+            case 4, 6, 9, 11 -> daysInMonth = 30;
+            default -> daysInMonth = 31;
+        }
+
+        System.out.println(daysInMonth);
     }
-
 }
