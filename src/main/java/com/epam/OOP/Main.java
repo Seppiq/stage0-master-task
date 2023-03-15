884de279cc34bb27982fad54db4b145e34afff8a
Kirill Semak <mvrch31@gmail.com>
2023-03-15 10:40:58 +0300

    feat: added all remaining files

---
+++ src/main/java/com/epam/OOP/Main.java
@@ -0,0 +1,10 @@
+package com.epam.OOP;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Bird bird = new Bird();
+        System.out.println(dog.getDescription());
+        System.out.println(bird.getDescription());
+    }
+}
